35c3858ffcac250dc2f42ca899a3eee3329acbeb
stCarolas <stcarolas@gmail.com>
2025-03-17 13:46:06 +0300

    add uploaded fonts

--- src/main/java/io/github/opendonationassistant/font/view/FontController.java
+++ src/main/java/io/github/opendonationassistant/font/view/FontController.java
@@ -1,18 +1,27 @@
 package io.github.opendonationassistant.font.view;
 
-import java.util.List;
-import java.util.Map;
-
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import jakarta.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller("/fonts")
 public class FontController {
 
   private final Map<String, List<String>> fontsCache;
+
+  private final Map<String, Map<String, String>> storedFonts = Map.of(
+    "chava-regular",
+    Map.of("truetype", "https://cdn.oda.digital/fonts/Chava-Regular.ttf"),
+    "steelfish-regular",
+    Map.of("opentype", "https://cdn.oda.digital/fonts/Steelfish%20Rg.otf"),
+    "steelfish-outline",
+    Map.of("opentype", "https://cdn.oda.digital/fonts/Steelfish%20Outline.otf")
+  );
 
   @Inject
   public FontController(Map<String, List<String>> fontsCache) {
@@ -22,10 +31,19 @@ public class FontController {
   @Get
   @Secured(SecurityRule.IS_ANONYMOUS)
   public List<FontDto> listFonts() {
-    final List<String> fonts = fontsCache.getOrDefault("fontsource", List.of());
-    return fonts.stream()
+    var fonts = new ArrayList<FontDto>();
+    var stored = storedFonts
+      .entrySet()
+      .stream()
+      .map(entry -> new FontDto(entry.getKey(), "stored", entry.getValue()))
+      .toList();
+    var google = fontsCache
+      .getOrDefault("fontsource", List.of())
+      .stream()
       .map(font -> new FontDto(font, "google"))
       .toList();
+    fonts.addAll(stored);
+    fonts.addAll(google);
+    return fonts;
   }
-
 }
--- src/main/java/io/github/opendonationassistant/font/view/FontDto.java
+++ src/main/java/io/github/opendonationassistant/font/view/FontDto.java
@@ -1,5 +1,8 @@
 package io.github.opendonationassistant.font.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
@@ -7,10 +10,16 @@ public class FontDto {
 
   private String name;
   private String type;
+  private Map<String,String> sources;
 
   public FontDto(String name, String type) {
+    this(name, type, new HashMap<>());
+  }
+
+  public FontDto(String name, String type, Map<String, String> sources){
     this.name = name;
     this.type = type;
+    this.sources = sources;
   }
 
   public String getName() {
@@ -19,5 +28,9 @@ public class FontDto {
 
   public String getType() {
     return type;
+  }
+
+  public Map<String, String> getSources(){
+    return this.sources;
   }
 }
