694a6c171403ecf97b33f4c2710bb30ef49c58e4
stCarolas <stcarolas@gmail.com>
2025-05-13 10:06:15 +0300

    add doom font

--- src/main/java/io/github/opendonationassistant/font/view/FontController.java
+++ src/main/java/io/github/opendonationassistant/font/view/FontController.java
@@ -24,7 +24,7 @@ public class FontController {
     "zrnic-regular",
     Map.of("opentype", "https://cdn.oda.digital/fonts/zrnic%20rg.otf"),
     "doom",
-    Map.of("truetype", "http://cdn.oda.digital/fonts/Doom2016Text-GOlBq.ttf")
+    Map.of("truetype", "https://cdn.oda.digital/fonts/Doom2016Text-GOlBq.ttf")
   );
 
   @Inject
