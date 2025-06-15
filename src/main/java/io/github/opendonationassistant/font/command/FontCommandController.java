e281ae522e093a5e027e0c77d10c67f781fe7b9d
stCarolas <stcarolas@gmail.com>
2025-06-15 20:16:36 +0300

    add uploading font

--- src/main/java/io/github/opendonationassistant/font/command/FontCommandController.java
+++ src/main/java/io/github/opendonationassistant/font/command/FontCommandController.java
@@ -1,18 +1,25 @@
 package io.github.opendonationassistant.font.command;
 
 import io.github.opendonationassistant.font.integration.FontsourceApi;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Put;
+import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.multipart.CompletedFileUpload;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
+import jakarta.annotation.Nullable;
 import jakarta.inject.Inject;
 import java.util.List;
 import java.util.Map;
 
-@Controller("/fonts/commands")
+@Controller
 public class FontCommandController {
 
   private FontsourceApi fontsource;
@@ -27,7 +34,7 @@ public class FontCommandController {
     this.fontsCache = fontsCache;
   }
 
-  @Post
+  @Post("/fonts/commands/refresh")
   @Secured(SecurityRule.IS_ANONYMOUS)
   @ExecuteOn(TaskExecutors.BLOCKING)
   public void refresh(@Body RefreshCommand command) {
---
+++ src/main/java/io/github/opendonationassistant/font/command/UploadCommand.java
@@ -0,0 +1,40 @@
+package io.github.opendonationassistant.font.command;
+
+import io.github.opendonationassistant.commons.logging.ODALogger;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.Put;
+import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.multipart.CompletedFileUpload;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.Authentication;
+import io.micronaut.security.rules.SecurityRule;
+import jakarta.annotation.Nullable;
+import java.util.Map;
+import org.apache.fontbox.ttf.TTFParser;
+import org.apache.fontbox.ttf.TrueTypeFont;
+
+@Controller
+public class UploadCommand {
+
+  private final ODALogger log = new ODALogger(this);
+
+  @Secured(SecurityRule.IS_AUTHENTICATED)
+  @Put(
+    value = "/fonts/commands/upload",
+    consumes = { MediaType.MULTIPART_FORM_DATA },
+    produces = { MediaType.TEXT_PLAIN }
+  )
+  public void put(
+    @PathVariable String name,
+    @Nullable @QueryValue("public") Boolean isPublic,
+    CompletedFileUpload file,
+    Authentication auth
+  ) throws Exception {
+    var parser = new TTFParser();
+    final TrueTypeFont font = parser.parseEmbedded(file.getInputStream());
+    font.getName();
+    log.info("Uploading font", Map.of("name", font.getName()));
+  }
+}
