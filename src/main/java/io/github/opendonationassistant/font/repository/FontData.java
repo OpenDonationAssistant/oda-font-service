c8563a599b1481c96e23ce14baf10b7db860c230
stCarolas <stcarolas@gmail.com>
2025-06-19 22:33:20 +0300

    add db integration

--- src/main/java/io/github/opendonationassistant/font/repository/FontData.java
+++ src/main/java/io/github/opendonationassistant/font/repository/FontData.java
@@ -1,12 +1,15 @@
 package io.github.opendonationassistant.font.repository;
 
 import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.MappedProperty;
 import io.micronaut.serde.annotation.Serdeable;
 
+@MappedEntity("font")
 @Serdeable
 public record FontData(
   @Id String id,
-  String recipientId,
+  @MappedProperty("recipient_id") String recipientId,
   String name,
   String url
 ) {}
---
+++ src/main/resources/db/migration/V1__create-payment-table.sql
@@ -0,0 +1,6 @@
+create table font (
+  id varchar(255),
+  recipient_id varchar(255),
+  name varchar(255),
+  url varchar(255),
+);
