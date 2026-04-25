e35306faa87cb9cbac8d42883af71350e8680a93
stCarolas <stcarolas@gmail.com>
2026-04-25 06:49:03 +0300

    add catching error for font refreshing

--- src/main/java/io/github/opendonationassistant/Startup.java
+++ src/main/java/io/github/opendonationassistant/Startup.java
@@ -1,17 +1,20 @@
 package io.github.opendonationassistant;
 
+import io.github.opendonationassistant.commons.logging.ODALogger;
 import io.github.opendonationassistant.font.command.Refresh;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.runtime.server.event.ServerStartupEvent;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import java.util.Map;
 
 @Singleton
 @Requires(env = "standalone")
 public class Startup implements ApplicationEventListener<ServerStartupEvent> {
 
   private final Refresh refresh;
+  private ODALogger log = new ODALogger(this);
 
   @Inject
   public Startup(final Refresh refresh) {
@@ -20,6 +23,10 @@ public class Startup implements ApplicationEventListener<ServerStartupEvent> {
 
   @Override
   public void onApplicationEvent(ServerStartupEvent event) {
-    refresh.refresh();
+    try {
+      refresh.refresh();
+    } catch (Exception e) {
+      log.error("Error refreshing fonts", Map.of("error", e.getMessage()));
+    }
   }
 }
